d94bfa04503a947461abbebd0acbfeaa045b05ea
BLACKY <blacky@BlackyLeaders-MacBook-Air.local>
2018-03-10 15:38:02 +0800

    后台视频管理、订单管理、权限管理；

--- cms.fitness.dayaartist.com/App/Admin/Controller/AdminuserController.class.php
+++ cms.fitness.dayaartist.com/App/Admin/Controller/AdminuserController.class.php
@@ -5,12 +5,13 @@ class AdminuserController extends PublicController{
 	//*************************
 	// 管理员的管理
 	//*************************
-	public function adminuser(){
+	public function adminuser()
+	{
 		$id=(int)$_GET['id'];
 
 		$count=M('adminuser')->count();
 
-		$userlist=M('adminuser as a')->field('a.id,a.name,a.addtime,role_name')->join('rbac_role as r on a.role_id=r.id')->select();
+		$userlist=M('adminuser as a')->field('a.id,a.name,a.addtime,role_name,del')->join('rbac_role as r on a.role_id=r.id')->select();
 		//print_r($userlist);die;
 		foreach ($userlist as $k => $v) {
 			$userlist[$k]['addtime']=date("Y-m-d H:i",$v['addtime']);
@@ -27,7 +28,8 @@ class AdminuserController extends PublicController{
 	//*************************
 	// 管理员&商家会员的添加
 	//*************************
-	public function add(){
+	public function add()
+	{
 		//==================
 		// GET到的参数集合
 		//==================
@@ -42,7 +44,7 @@ class AdminuserController extends PublicController{
 		        'name' => trim($_POST['name']),
 				'pwd' => MD5(MD5($_POST['password'])),
 				'role_id' => (int)($_POST['role_id']),
-				'suppiler_id'=>(int)($_POST['supplier_id']),
+				'supplier_id'=>(int)($_POST['supplier_id']),
 		    );
 			if(intval($_POST['admin_id'])>0){
 				//更新
@@ -71,12 +73,14 @@ class AdminuserController extends PublicController{
 			}
 		}
 		//id>0则为编辑状态
-		$adminuserinfo = $id>0 ? M('adminuser')->where("id=$id")->find():""; 
+		$adminuserinfo = $id > 0 ? M('adminuser')->where("id=$id")->find():""; 
 		$supplier = M('box_supplier')->select();
+		$role = M('rbac_role')->select();
 		//=============
 		//将变量输出
 		//=============
 		$this->assign('id',$id);
+		$this->assign('role',$role);
 		$this->assign('supplier',$supplier);
 		$this->assign('adminuserinfo',$adminuserinfo);
 		$this->display();
@@ -84,7 +88,7 @@ class AdminuserController extends PublicController{
 
 	public function del()
 	{
-		$id = intval($_REQUEST['did']);
+		$id = intval($_REQUEST['id']);
 		$info = M('adminuser')->where('id='.intval($id))->find();
 		if (!$info) {
 			$this->error('参数错误.'.__LINE__);
@@ -96,13 +100,13 @@ class AdminuserController extends PublicController{
 			exit();
 		}
 
-		if ($info['del']==1) {
+		if ($info['del']==2) {
 			$this->redirect('Adminuser/adminuser',array('page'=>intval($_REQUEST['page'])));
 			exit();
 		}
 
 		$data=array();
-		$data['del'] = 1;
+		$data['del'] = 2;
 		$up = M('adminuser')->where('id='.intval($id))->save($data);
 		if ($up) {
 			$this->redirect('Adminuser/adminuser',array('page'=>intval($_REQUEST['page'])));
@@ -114,11 +118,39 @@ class AdminuserController extends PublicController{
 	}
 
 	//*************************
-	// 角色权限
+	// 角色列表
 	//*************************
-	public function role_auth()
+	public function role()
 	{
+		$role = M('rbac_role')->select();
+		foreach ($role as $key => $value) {
+			$role[$key]['create_time'] = date('Y-m-d H:i:s',$value['create_time']);
+		}
+		$this->assign('role',$role);
 		$this->display();
+	}
+
+	//*************************
+	// 添加角色
+	//*************************
+	public function add_role()
+	{	
+		if (IS_POST) {
+			$data = I('post.');
+			$role_data = M('rbac_role')->getField('role_name',true);
+			if (in_array($data['role_name'], $role_data)) {
+				$this->error('该角色已添加');
+			}
+			$info = array('role_name'=>$data['role_name'],'status'=>1,'create_time'=>time());
+			$res = M('rbac_role')->add($info);
+			if ($res) {
+				$this->success('添加成功',U('adminuser/role'));
+			}else{
+				$this->error('添加失败，请重新添加');
+			}
+		}else{
+			$this->display();
+		}
 	}
 
 	//*************************
@@ -131,24 +163,32 @@ class AdminuserController extends PublicController{
 				if ($_POST['submit']==true) {
 
 					$data = I('post.');
-					//查看一级权限
-					$auth_info = M('rbac_auth')->where('id='.$data['auth_id'])->find();
+					if (!isset($data['auth_id']) || $data['auth_id'] == -1) throw new \Exception('请选择权限');
 
-					$auth_one = M('rbac_role_auth')->where('role_id='.$data['role_id'].' AND auth_id='.$auth_info['auth_pid'])->find();
+					//print_r($data);die;
+					$role_auth = M('rbac_role_auth');
+					//查看一级权限
+					$auth_one = $role_auth->where('role_id='.$data['role_id'].' AND auth_id='.$data['auth_pid'])->find();
 
 					if (!$auth_one) {  //是否添加过一级权限
-						$auth_array = array('role_id'=>$data['role_id'],'auth_id'=>$auth_info['auth_pid']);
-						$auth_one_res = M('rbac_role_auth')->add($auth_array);
+						$auth_array = array('role_id'=>$data['role_id'],'auth_id'=>$data['auth_pid']);
+						$auth_one_res = $role_auth->add($auth_array);
 
 						if (!$auth_one_res) {
 							throw new \Exception('添加一级权限失败');
 						}
 					}
 
-					$res = M('rbac_role_auth')->add($data);
+					$autn_check = $role_auth->where('role_id='.$data['role_id'].' AND auth_id='.$data['auth_id'])->find();
+					if ($auth_check) {
+						if (!$auth_one_res) {
+							throw new \Exception('该权限已添加');
+						}
+					}
+					$res = $role_auth->add($data);
 					
 					if ($res) {
-						$this->success('保存成功！',U('adminuser/role_auth'));
+						$this->success('保存成功！',U('adminuser/role'));
 					}else{
 
 						throw new \Exception('保存失败！');
@@ -164,7 +204,11 @@ class AdminuserController extends PublicController{
 			
 		}else{
 
+			$id = I('get.id');
+			$role = M('rbac_role')->where('id='.$id)->find();
 			$auth = M('rbac_auth')->where('auth_pid=0')->select();
+
+			$this->assign('role',$role);
 			$this->assign('auth',$auth);
 			$this->display();
 
@@ -172,9 +216,70 @@ class AdminuserController extends PublicController{
 	}
 
 	//*************************
+	// 权限列表
+	//*************************
+	public function auth()
+	{
+		$auth = M('rbac_auth')->select();
+		foreach ($auth as $key => $v) {
+			$auth[$key]['create_time'] = date('Y-m-d H:i:s',$v['create_time']);
+			$auth[$key]['auth'] = $v['auth_c'].'/'.$v['auth_a'];
+		}
+		$this->assign('auth',$auth);
+		$this->display();
+	}
+
+	//*************************
+	// 添加权限
+	//*************************
+	public function add_auth()
+	{
+		$rbac_auth = M('rbac_auth');
+		if (IS_POST) {
+			
+			$data = I('post.');
+			//print_r($data);die;
+			if ($data['auth_pid'] == 0) {
+
+				$where = array('auth_c'=>$data['auth_c'],'auth_pid'=>0);
+				$auth_check = $rbac_auth->where($where)->find();
+
+			}else if($data['auth_pid'] > 0){
+
+				$where = array('auth_c'=>$data['auth_c'],'auth_a'=>$data['auth_a']);
+				$auth_check = $rbac_auth->where($where)->find();
+
+			}else{
+				$this->error('编辑失败');
+			}
+
+			if (!empty($auth_check)) $this->error('该权限已存在');
+
+			$add_data = array('auth_name'=>$data['auth_name'],'auth_pid'=>$data['auth_pid'],'auth_c'=>$data['auth_c'],'auth_a'=>$data['auth_a'],'create_time'=>time());
+
+
+			$res = $rbac_auth->add($add_data);
+			if ($res) {
+				$this->success('编辑成功',U('adminuser/auth'));
+			}else{
+				$this->error();
+			}
+		}else{
+			$data = $rbac_auth->where('auth_pid=0')->select();
+			foreach ($data as $key => $v) {
+				$data[$key]['create_time'] = date('Y-m-d H:i:s',$v['create_time']);
+			}
+			$this->assign('data',$data);
+			$this->display();
+		}
+		
+	}
+
+	//*************************
 	// 二级权限
 	//*************************
-	public function two_auth(){
+	public function two_auth()
+	{
 		$auth_pid = I('get.auth_pid');
 		$data = array('data'=>'','message'=>'','status'=>'');
 		$auth = M('rbac_auth')->where('auth_pid='.$auth_pid)->select();
@@ -185,4 +290,6 @@ class AdminuserController extends PublicController{
 		}
 		echo json_encode($data);
 	}
+
+
 }
