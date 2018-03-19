054fcad16630754b3cd7e0aad71790a2d492df7c
BLACKY <blacky@BlackyLeaders-MacBook-Air.local>
2018-03-19 23:28:35 +0800

    场馆课程轮播图片；

--- miniProgress/utils/config.js
+++ miniProgress/utils/config.js
@@ -5,7 +5,7 @@ class Config {
 	}
 }
 
-Config.restUrl = 'https://api.blocktrain.com.cn/index.php/api/v2/';
+Config.restUrl = 'https://api.fitness.dayaartist.com/index.php/api/v2/';
 Config.onPay = true;  //是否启用支付
 
 Config.cityName = '北京市';
