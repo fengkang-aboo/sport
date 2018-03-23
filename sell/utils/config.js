e7edc1b9d583a7adec497bdd98d1f8d18816e179
kang <kang.feng@blackarcgroup.com>
2018-03-23 14:05:15 +0800

    加入商家版

--- sell/utils/config.js
+++ sell/utils/config.js
@@ -5,10 +5,10 @@ class Config {
 	}
 }
 
-Config.restUrl = 'https://api.blocktrain.com.cn/index.php/api/v2/';
+Config.restUrl = 'https://api.dayaartist.com/index.php/api/v2/';
 Config.onPay = true;  //是否启用支付
 
 Config.cityName = '北京市';
 Config.cityCode = 0;
 
-export { Config };  
+export { Config };
