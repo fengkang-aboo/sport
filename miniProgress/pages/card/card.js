9076caf075c66dc97de32dd03413881c515b0291
LittleLittleJun <1710950629@qq.com>
2018-04-30 15:31:56 +0800

    红包页面

--- miniProgress/pages/card/card.js
+++ miniProgress/pages/card/card.js
@@ -1,66 +1,36 @@
-// pages/card/card.js
+import { Card } from 'card-model.js';
+
+var card = new Card();  //实例化 商品详情 对象
+
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    currentTabsIndex: 0,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-  
+    // this.setData({
+    //   id: options.id
+    // })
+    // this._loadData();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  /*加载所有数据*/
+
+  //切换详情面板
+  onTabsItemTap: function (event) {
+    var index = card.getDataSet(event, 'index');
+    this.setData({
+      currentTabsIndex: index
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
+  //分享效果
   onShareAppMessage: function () {
-  
+    return {
+      //   title: this.data.product.name
+    }
   }
+
 })
+
+
